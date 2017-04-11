7c600ebfc46bcc934363c8d7f5503f95e57cfd19
“常玉龙” <cyl455286786@gmail.com>
2017-04-11 20:42:46 +0800

    add validation to title

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,6 +3,10 @@ class JobsController < ApplicationController
     @jobs = Job.all
   end
 
+  def show
+    @job = Job.find(params[:id])
+  end
+
   def new
     @job = Job.new
   end
@@ -13,8 +17,12 @@ class JobsController < ApplicationController
 
   def create
     @job = Job.new(job_params)
-    @job.save
+
+    if @job.save
       redirect_to jobs_path
+    else
+      render :new
+    end
   end
 
   def update
@@ -22,6 +30,13 @@ class JobsController < ApplicationController
     @job.update(job_params)
     redirect_to jobs_path, notice:'Update Success'
   end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path, alert:"Job deteled"
+  end
+
 
   private
 
