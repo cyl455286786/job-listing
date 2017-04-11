079062cbbf1452ecf90782a90dc11bad6273e01a
“常玉龙” <cyl455286786@gmail.com>
2017-04-11 20:04:55 +0800

    implement jobs#edit

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,6 +7,10 @@ class JobsController < ApplicationController
     @job = Job.new
   end
 
+  def edit
+    @job = Job.find(params[:id])
+  end
+
   def create
     @job = Job.new(job_params)
     @job.save
