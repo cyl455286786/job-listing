3385c1b57caee675d59b5499c827f1a8b5c9d2eb
“常玉龙” <cyl455286786@gmail.com>
2017-04-11 20:11:58 +0800

    implement jobs#update

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -17,6 +17,12 @@ class JobsController < ApplicationController
       redirect_to jobs_path
   end
 
+  def update
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+    redirect_to jobs_path, notice:'Update Success'
+  end
+
   private
 
   def job_params
